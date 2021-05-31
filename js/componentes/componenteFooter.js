f68c6754e35e5b8f7c9a5f9cc0b5e92fb6df9c79
luchoxx87 <neoluis87@gmail.com>
2021-05-31 11:37:39 -0300

    Agregada Release 2.2 en changelog
    También se agrega al footer.

--- js/componentes/componenteFooter.js
+++ js/componentes/componenteFooter.js
@@ -8,7 +8,7 @@ class Footer extends HTMLElement {
         <footer>
             <div class="footer">
             <br>
-            <p>Escuela Técnica N°12 Gral. José de San Martín <a href="changelog.html" title="Changelog">(v2.1)</a></p>
+            <p>Escuela Técnica N°12 Gral. José de San Martín <a href="changelog.html" title="Changelog">(v2.2)</a></p>
             <p>Página creada y diseñada por alumnos de 6° año Computación 2018</p>
             <a href="https://www.facebook.com/jose.desanmartin.927" id="fbIco" class="fab fa-facebook fa-3x"
                 title="Facebook Oficial de la Escuela" target="_blank"></a>
