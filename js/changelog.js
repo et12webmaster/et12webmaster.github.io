0956092c162292a022fe58bd9facbe2592e87d75
luchoxx87 <neoluis87@gmail.com>
2021-05-31 20:21:32 -0300

    Agregada v2.2.1

--- js/changelog.js
+++ js/changelog.js
@@ -141,11 +141,11 @@ const jonathanVelazquez = new Colaborador("Jonathan", "Velazquez", "Docente de C
 
 // INVOCACIÓN CAMBIOS
 
-/*agregarCambio("v2.3", "...", "2021-05-31",
-  [new DescripcionColaboracion("Implementación hoja de estilos para especialidades.", [luisDuran]),
-  new DescripcionColaboracion("Corrección menu desplegable", [agustinSantich]),
-  new DescripcionColaboracion("Corrección responsive changelog", [luisDuran, jonathanVelazquez])]
-);*/
+agregarCambio("v2.2.1", "https://github.com/et12webmaster/et12webmaster.github.io/releases/tag/2.2.1", "2021-05-31",
+  [new DescripcionColaboracion("Corrección menu desplegable", [agustinSantich]),
+  new DescripcionColaboracion("Corrección responsive changelog", [luisDuran, jonathanVelazquez]),
+  new DescripcionColaboracion("Implementación hoja de estilos para especialidades.", [luisDuran])]
+);
 
 agregarCambio("v2.2", "https://github.com/et12webmaster/et12webmaster.github.io/releases/tag/2.2", "2021-05-31",
   [new DescripcionColaboracion("Agregada página de changelog.", [luisDuran]),
