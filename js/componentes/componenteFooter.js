116b1d2dbf683141f20bacb2dee4c44d9802e00a
Medina Lucas <medina.lucas.e@gmail.com>
2022-09-07 21:15:31 -0300

    Footer 2.0
    Incorporado diseño responsive del rediseñado footer

--- js/componentes/componenteFooter.js
+++ js/componentes/componenteFooter.js
@@ -7,15 +7,29 @@ class Footer extends HTMLElement {
         this.innerHTML = `
         <footer>
             <div class="footer">
-            <br>
-            <p>Escuela Técnica N°12 Gral. José de San Martín <a href="changelog.html" title="Changelog">(v2.2.1)</a></p>
-            <p>Página creada y diseñada por alumnos de 6° año Computación 2018</p>
-            <a href="https://www.facebook.com/jose.desanmartin.927" id="fbIco" class="fab fa-facebook fa-3x"
-                title="Facebook Oficial de la Escuela" target="_blank"></a>
-            <a href="https://www.instagram.com/escuelatec12/" id="instaIco" class="fab fa-instagram fa-3x"
-                title="Instagram Oficial de la Escuela" target="_blank"></a>
-            <a href="https://github.com/et12webmaster/et12webmaster.github.io" id="githubIco" class="fab fa-github fa-3x"
-                title="Repositorio del Sitio" target="_blank"></a>
+                <table> <tr>
+                    <td class="footer-col1">
+                        <div class="footer-info-block">
+                            <div class="footer-info">
+                                Av.Del Libertador 238 - CABA (1001)<br>
+                                Tel: 4327-3942 / 4328-4433 <br>
+                                Mail institucional: <a href="mailto:det_12_de1@bue.edu.ar?subject=Mail via web: ">det_12_de1@bue.edu.ar</a> <br>
+                                <b>Toda la información de contacto <a href="contacto.html"> aquí </a> </b>
+                            </div>
+                        </div>
+                    </td>
+
+                    <td class="footer-col2">
+                        <div class="footer-icon-block">
+                            <a href="https://www.facebook.com/jose.desanmartin.927" id="fbIco" class="fab fa-facebook fa-3x footer-icon"
+                                title="Facebook Oficial de la Escuela" target="_blank"></a>
+                            <a href="https://www.instagram.com/escuelatec12/" id="instaIco" class="fab fa-instagram fa-3x footer-icon"
+                                title="Instagram Oficial de la Escuela" target="_blank"></a>
+                            <a href="https://github.com/et12webmaster/et12webmaster.github.io" id="githubIco" class="fab fa-github fa-3x footer-icon"
+                                title="Repositorio del Sitio" target="_blank"></a>
+                        </div>
+                    </td>
+                </tr> </table>
             </div>            
         </footer>
         `;
